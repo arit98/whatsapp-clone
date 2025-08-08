95f1cb614d274ad35081ee81c37c519499adbe6f
arit98 <aritradaschowdhury@gmail.com>
2025-08-08 21:11:33 +0530

    check db connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,13 +7,17 @@ interface ConnectionObject {
 const connection: ConnectionObject = {}
 
 async function dbConnect(): Promise<void> {
+    const {user_name, password} = process.env;
+
     if(connection.isConnect) {
         console.log("Database is already connected");
         return;
     }
 
     try {
-        const mongoUri = process.env.MONGODB_URI;
+        const mongoUri = `mongodb+srv://${user_name}:${password}@whatsapp.dtfu88z.mongodb.net/`;
+
+        console.log(mongoUri);
         
         if (!mongoUri) {
             throw new Error("MONGODB_URI environment variable is not set. Please check your .env.local file.");
